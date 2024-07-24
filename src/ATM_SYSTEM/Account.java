da4372295407e2dffd48c9b709b828f887983c0c
shanmengqi <shanmengqi@gmail.com>
2024-07-25 06:29:31 +0800

    Basic architecture

---
+++ src/ATM_SYSTEM/Account.java
@@ -0,0 +1,69 @@
+package ATM_SYSTEM;
+
+public class Account {
+    private String userName;
+    private String userID;
+    private char gender;
+    private String password;
+    private double balance;
+    private double limitation;
+
+    public Account(String userName, char gender, String password, double balance, double limitation, String userID) {
+        this.userName = userName;
+        this.gender = gender;
+        this.password = password;
+        this.balance = balance;
+        this.limitation = limitation;
+        this.userID = userID;
+    }
+
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getLimitation() {
+        return limitation;
+    }
+
+    public void setLimitation(double limitation) {
+        this.limitation = limitation;
+    }
+}
---
+++ src/ATM_SYSTEM/AppTest.java
@@ -0,0 +1,4 @@
+package ATM_SYSTEM;
+
+public class AppTest {
+}
