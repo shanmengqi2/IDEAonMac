fcfa2eedda9bae7f33d7148ce767caba6d283108
shanmengqi2 <69161111+shanmengqi2@users.noreply.github.com>
2024-07-25 08:49:22 +0800

    Update ATM.java

--- src/ATM_SYSTEM/ATM.java
+++ src/ATM_SYSTEM/ATM.java
@@ -1,4 +1,43 @@
 package ATM_SYSTEM;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ATM {
+    ArrayList<Account> accountList= new ArrayList<>();
+
+    public ATM() {
+
+    }
+
+    //欢迎页
+    public void homePage(){
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("欢迎进入黑马银行ATM系统");
+            System.out.println("1、用户登录");
+            System.out.println("2、用户开户");
+            switch(sc.nextLine()){
+                case "1":userLogin();
+                    break;
+                case "2":addAccount();
+                    break;
+                default:
+                    System.out.println("请输入正确的指令");
+            }
+        }
+    }
+
+    //用户登录
+    public void userLogin(){
+
+    }
+
+    //用户开户
+    public void addAccount(){
+
+    }
+
+
 }
